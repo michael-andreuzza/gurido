8b8a244ef848b70bb1b22bf20fb06cfa8b3b5d9d
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-11-15 14:37:53 +0200

    first push

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://neuepower.com/",
   integrations: [tailwind(), sitemap()]
 });
