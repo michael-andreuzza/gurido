7c855349da1fb6abb0234f13a1f01d3dc86cd1ad
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-11-16 12:28:23 +0200

    first push

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://qquubb.com/",
+  site: "https://monomod.design/",
   integrations: [tailwind(), sitemap()]
 });
